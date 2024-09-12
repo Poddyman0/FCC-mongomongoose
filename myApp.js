a55e4b5502307d1704005605668beea96c376677
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-09-12 13:36:42 +0100

    env changed

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
 //run npm start to test.
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
